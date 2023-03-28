9acb043ab673294a126cefce335ebe8e72214824
1yoon0 <proof0503@gmail.com>
2023-03-28 15:29:29 +0900

    new branch

---
+++ sfow_lite/src/main/java/com/yeonoo/pp/controller/ProuctController.java
@@ -0,0 +1,13 @@
+package com.yeonoo.pp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProuctController {
+	
+	@GetMapping("/product/pp")
+	public String ppMain() {
+		return "pp/product";
+	}
+}
