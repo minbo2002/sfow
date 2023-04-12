d84cd3ebcbf6081dea9381787ed96844358e35f0
오하늘 <ohnbgawk89@naver.com>
2023-04-12 16:27:37 +0900

    0412 작업지시마무리 커밋완료

--- sfow_lite/src/main/java/com/yeonoo/sfow/basicinfo/controller/UserInfoController.java
+++ sfow_lite/src/main/java/com/yeonoo/sfow/basicinfo/controller/UserInfoController.java
@@ -25,6 +25,10 @@ public class UserInfoController {
 	
 	private final UserInfoService userInfoService;
 	
+   @RequestMapping("/sessionLogin")
+   public String sessionLogin() throws Exception {
+      return "basicinfo/sessionLogin";
+   }
 	
 	//로그인페이지
 	@RequestMapping("/login")
